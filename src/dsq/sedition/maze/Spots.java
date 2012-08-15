dcc34fdb58b40ffc4ec6293e2e63585131381661
dsq <dsq@server>
2012-08-16 02:05:22 +1000

    almost playable ... code need a heap of cleanup though

--- src/dsq/sedition/maze/Spots.java
+++ src/dsq/sedition/maze/Spots.java
@@ -4,6 +4,7 @@ public class Spots {
     private Spots()  { }
     
     public static int hash(final Spot spot, int width) {
-        return spot.z * width + spot.x;
+        // FIX 16/08/12 The plus one and minus one problems are getting out of hand.
+        return spot.z * (width + 1) + spot.x;
     }
 }
--- test/dsq/sedition/maze/RandomLevelTest.java
+++ test/dsq/sedition/maze/RandomLevelTest.java
@@ -1,11 +1,20 @@
 package dsq.sedition.maze;
 
+import dsq.sedition.collision.Line2D;
 import junit.framework.TestCase;
+
+import java.util.List;
 
 public class RandomLevelTest extends TestCase {
     public void test() {
-        for (int i = 0; i < 200; i++) {
-            new RandomLevel(10, 10);
+//        for (int i = 0; i < 200; i++) {
+//            new RandomLevel(10, 10);
+//        }
+        final RandomLevel level = new RandomLevel(3, 3);
+        final List<Line2D> actual = level.walls();
+        for (Line2D line2D : actual) {
+            System.out.println("line2D = " + "("  + (int)line2D.x1 + ", " + (int)line2D.z1 + ") -> (" +
+                (int)line2D.x2 + ", " + (int)line2D.z2 + ")");
         }
     }
 }
